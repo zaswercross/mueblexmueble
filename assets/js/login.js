abab31ffe2d015761be33551ace2e6049f04fc31
Eleazar Estrada <eleazar.estrada@eficasia.com>
2021-02-26 18:22:20 -0600

    asldkfjasñldfkj

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,7 +1,27 @@
-
+if (sessionStorage.getItem("login") == 1) {
+    document.querySelector(".home-seccion").classList.remove("d-none")
+    document.querySelector(".wellcome-section").classList.remove("d-none")
+    document.querySelector(".chat-section").classList.add("d-none")
+    document.querySelector(".body-login").classList.add("d-none")
+    document.querySelector("#login").classList.add("d-none")
+    document.querySelector("#register").classList.add("d-none")
+} else {
+    document.querySelector(".home-seccion").classList.add("d-none")
+    document.querySelector(".wellcome-section").classList.add("d-none")
+    document.querySelector(".chat-section").classList.add("d-none")
+    document.querySelector(".body-login").classList.remove("d-none")
+    document.querySelector("#login").classList.remove("d-none")
+    document.querySelector("#register").classList.add("d-none")
+}
 document.querySelector("#formLogin").addEventListener("submit", e => {
     e.preventDefault();
-
+    sessionStorage.setItem("login", 1);
+    document.querySelector(".home-seccion").classList.remove("d-none")
+    document.querySelector(".wellcome-section").classList.remove("d-none")
+    document.querySelector(".chat-section").classList.add("d-none")
+    document.querySelector(".body-login").classList.add("d-none")
+    document.querySelector("#login").classList.add("d-none")
+    document.querySelector("#register").classList.add("d-none")
 });
 
 document.querySelector("#crearCuenta").addEventListener("click", e => {
@@ -21,3 +41,58 @@ document.querySelector(".iniciarSesion").addEventListener("click", e => {
     // document.querySelector(".overlay").style.background = "#ECE2CF94";
     document.querySelector(".overlay").style.background = "#46321C7A";
 });
+
+const buttonsDrops = document.querySelectorAll(".dropdown-toggler");
+
+for (let index = 0; index < buttonsDrops.length; index++) {
+    const element = buttonsDrops[index];
+    element.addEventListener("click", e => {
+
+        console.log($(e.target).parent().children(".dropdown-menu"))
+        if (e.target.parentElement.children[3].classList.contains("show")) {
+            $(e.target).parent().children(".dropdown-menu").removeClass("show")
+            return false;
+        } else {
+            $(e.target).parent().children(".dropdown-menu").addClass("show")
+            return false;
+        }
+    })
+}
+
+const salirButtons = document.querySelectorAll(".salir");
+
+for (let index = 0; index < salirButtons.length; index++) {
+    const element = salirButtons[index];
+    element.addEventListener("click", e => {
+        sessionStorage.setItem("login", 0);
+        document.querySelector(".home-seccion").classList.add("d-none")
+        document.querySelector(".wellcome-section").classList.add("d-none")
+        document.querySelector(".chat-section").classList.add("d-none")
+        document.querySelector(".body-login").classList.remove("d-none")
+        document.querySelector("#login").classList.remove("d-none")
+        document.querySelector("#register").classList.add("d-none")
+    })
+}
+const homeButtons = document.querySelectorAll(".home");
+
+for (let index = 0; index < homeButtons.length; index++) {
+    const element = homeButtons[index];
+    element.addEventListener("click", e => {
+        sessionStorage.setItem("login", 0);
+        document.querySelector(".home-seccion").classList.remove("d-none")
+        document.querySelector(".wellcome-section").classList.remove("d-none")
+        document.querySelector(".chat-section").classList.add("d-none")
+        document.querySelector(".body-login").classList.add("d-none")
+        document.querySelector("#login").classList.add("d-none")
+        document.querySelector("#register").classList.add("d-none")
+    })
+}
+
+document.querySelector(".chat-active").addEventListener("click", e => {
+    document.querySelector(".home-seccion").classList.remove("d-none")
+    document.querySelector(".wellcome-section").classList.add("d-none")
+    document.querySelector(".chat-section").classList.remove("d-none")
+    document.querySelector(".body-login").classList.add("d-none")
+    document.querySelector("#login").classList.add("d-none")
+    document.querySelector("#register").classList.add("d-none")
+})
